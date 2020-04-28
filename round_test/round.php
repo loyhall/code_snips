a9967188fc22f3d0793643bb2c1dac693e139e8d
loyhall <4481155+loyhall@users.noreply.github.com>
2020-04-27 17:23:17 -0700

    round.php

---
+++ round_test/round.php
@@ -0,0 +1,18 @@
+<?php
+// ATOMIC_FUNCTION:  round($val, $precision)
+// LANG:  php
+// FUNCTION: round($val, $precision)
+
+$start_val = 1.2;
+$expected_end_val = 1;
+$precision = 0;
+$end_val = round($start_val,$precision);
+
+// TEST
+if($end_val == $expected_end_val) {
+	echo "PASS";
+} else {
+	echo "FAIL";
+}
+
+?>

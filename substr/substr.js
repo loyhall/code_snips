dbb4eb2db63d61de3bd4f3b27c0e91068a5b0c93
hdz1204 <35512107+hdz1204@users.noreply.github.com>
2020-04-30 10:48:59 -0700

    Add files via upload

---
+++ substr/substr.js
@@ -0,0 +1,17 @@
+// ATOMIC_FUNCTION:  substr($val, $start, $length)
+// LANG:  node.js
+// FUNCTION: substr($val, $start, $length) 
+
+var start_val = "abcdefg";
+var expected_end_val = "bcd";
+var start = 1;
+var length = 3;
+var end_val = start_val.substr(start,length);
+
+
+// TEST
+if(end_val == expected_end_val) {
+	console.log("PASS");
+} else {
+	console.log("FAIL");
+}

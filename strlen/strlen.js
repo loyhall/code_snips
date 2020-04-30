2e7924f665328a9ea1c6b6a191c526a90ce4c891
hdz1204 <35512107+hdz1204@users.noreply.github.com>
2020-04-30 10:48:47 -0700

    Add files via upload

---
+++ strlen/strlen.js
@@ -0,0 +1,14 @@
+// ATOMIC_FUNCTION:  strlen($val)
+// LANG:  node.js
+// FUNCTION: strlen($val) 
+
+var start_val = "abcdefg";
+var expected_end_val = 7;
+var end_val = start_val.length;
+
+// TEST
+if(end_val == expected_end_val) {
+	console.log("PASS");
+} else {
+	console.log("FAIL");
+}

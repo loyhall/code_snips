12959b99652eefda8c7f9586bd885fe9fe306fd6
hdz1204 <35512107+hdz1204@users.noreply.github.com>
2020-04-30 10:26:05 -0700

    Add files via upload

---
+++ ltrim/ltrim.java
@@ -0,0 +1,28 @@
+import java.util.regex.Pattern;
+
+public class ltrim {
+    private final static Pattern LTRIM = Pattern.compile("^\\s+");
+
+    public static String ltrim(String s) {
+        return LTRIM.matcher(s).replaceAll("");
+    }
+    public static void main(String args[]) {
+
+        // ATOMIC_FUNCTION:  trim($val)
+        // LANG:  java
+        // FUNCTION: trim($val)
+
+        String start_val = "  abcdefg  ";
+        String expected_end_val = "abcdefg  ";
+        String end_val = ltrim(start_val);
+
+
+
+        // TEST
+        if(end_val.equals(expected_end_val)){
+            System.out.println("PASS");
+        } else {
+            System.out.println("FAIL");
+        }
+    }
+}
---
+++ ltrim/ltrim.py
@@ -0,0 +1,22 @@
+def ltrim(start_val,del_char=" "):
+    return start_val.lstrip(del_char)
+
+
+# ATOMIC_FUNCTION:  ltrim(val)
+# LANG:  python
+# FUNCTION: ltrim(val) 
+
+start_val = "  abcdefg  "
+expected_end_val = "abcdefg  "
+end_val = ltrim(start_val)
+
+# TEST
+if(end_val == expected_end_val):
+    print("PASS")
+else:
+    print("FAIL")
+
+
+
+
+

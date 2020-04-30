83d5a93b8f1881d5f79c0c897f6061cdca3fe231
hdz1204 <35512107+hdz1204@users.noreply.github.com>
2020-04-30 10:49:50 -0700

    Add files via upload

---
+++ ltrim/ltrim.js
@@ -0,0 +1,18 @@
+// ATOMIC_FUNCTION:  ltrim($val)
+// LANG:  node.js
+// FUNCTION: ltrim($val) 
+function ltrim(str, chr) {
+    var rgxtrim = (!chr) ? new RegExp('^\\s+') : new RegExp('^'+chr+'+');
+    return str.replace(rgxtrim, '');
+}
+
+var start_val = "  abcdefg  ";
+var expected_end_val = "abcdefg  ";
+var end_val = ltrim(start_val);
+
+// TEST
+if(end_val == expected_end_val) {
+	console.log("PASS");
+} else {
+	console.log("FAIL");
+}
